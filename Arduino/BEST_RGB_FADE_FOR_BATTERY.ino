593bd1701749b47ae9332f42f91046a2f59fce92
Brett Reinhard <brett@reinhards.us>
2017-03-08 06:01:41 -0800

    Added LowPower lib
    Added LowPower library for power saving sleep, to allow for better battery savings

--- Arduino/BEST_RGB_FADE_FOR_BATTERY.ino
+++ Arduino/BEST_RGB_FADE_FOR_BATTERY.ino
@@ -2,6 +2,8 @@
 //Programmer: Brett Reinhard
 //Date: 10/20/2016
 //Description: Changes color of LED's within the Pumpkin, with smooth transitions.
+// Imports LowPower lib for power saving sleep
+#include LowPower.h
 int const Red = 3,  /* Pin 3 */ Blue = 5, /* Pin 5 */ Green = 6; /* Pin 6 */
 void RGB(int R, int G, int B) {
 analogWrite(Red,R); analogWrite(Green,G); analogWrite(Blue,B); delay(25);}
@@ -14,7 +16,8 @@ r = 0;g = 255;b = 0; for (int i = 0; i < 256; ++i){RGB(r,g,i);}
 r = 0;g = 255;b = 255; for (int i = 255; i >= 0; --i){RGB(r,i,b);}
 r = 0;g = 0;b = 255; for (int i = 0; i < 256; ++i){RGB(i,g,b);}
 r = 255;g = 0;b = 255; for (int i = 255; i >= 0; --i){RGB(r,g,i);}
-delay(1000);
+// Saves battery or power by shutting off multiple components
+LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
      }
  
 
